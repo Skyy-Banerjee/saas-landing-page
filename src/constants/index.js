868e0b29d17620c28419452f39528b735cdd9565
Soumadip <39thdecember@gmail.com>
2024-09-24 03:31:01 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,403 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/feature-1.png",
+    caption: "Easy integration",
+    title: "Work smarter not harder",
+    text: "With Xora, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "Watch the demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/feature-2.png",
+    caption: "Secure & trustworthy",
+    title: "Sleep easy, we got your back",
+    text: "Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Xora helps keeps your information secure and locked up tighter than Fort Knox.",
+    button: {
+      icon: "/images/docs.svg",
+      title: "Read the docs",
+    },
+  },
+];
+
+export const details = [
+  {
+    id: "0",
+    icon: "/images/detail-1.png",
+    title: "AI automated video editing",
+  },
+  {
+    id: "1",
+    icon: "/images/detail-2.png",
+    title: "Collaborate with your team",
+  },
+  {
+    id: "2",
+    icon: "/images/detail-3.png",
+    title: "Ultra fast cloud-engine",
+  },
+  {
+    id: "3",
+    icon: "/images/detail-4.png",
+    title: "24 / 7 Customer support",
+  },
+];
+
+export const faq = [
+  {
+    id: "0",
+    question: "How easy is it to setup Xora?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "1",
+    question: "Can I integrate Xora with other platforms?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "2",
+    question: "How often do you add new content?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "3",
+    question: "What your refund policy?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "4",
+    question: "Do you have corporate plans?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "5",
+    question: "What happens when I’m out of storage?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "6",
+    question: "Can I upgrade my plan?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "7",
+    question: "How do I invite my team?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "8",
+    question: "Do you offer training for individuals and teams?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+  {
+    id: "9",
+    question: "I’m locked out of my account what do I do?",
+    answer:
+      "Absolutely! Not only you can upgrade your plan at any time but you also get a prorated discount giving you maximum value for your subscription.",
+  },
+];
+
+export const plans = [
+  {
+    id: "0",
+    title: "Core",
+    priceMonthly: 19,
+    priceYearly: 12,
+    caption: "Best for solo creators",
+    features: [
+      "100MB Cloud storage",
+      "100+ prompt templates",
+      "5 projects",
+      "24/7 support",
+    ],
+    icon: "/images/circle.svg",
+    logo: "/images/plan-1.png",
+  },
+  {
+    id: "1",
+    title: "Overdrive",
+    priceMonthly: 79,
+    priceYearly: 59,
+    caption: "Most popular plan",
+    features: [
+      "All Starter features",
+      "1TB additional storage",
+      "Unlimited projects",
+      "Analytics",
+    ],
+    icon: "/images/triangle.svg",
+    logo: "/images/plan-2.png",
+  },
+  {
+    id: "2",
+    title: "Team",
+    priceMonthly: 39,
+    priceYearly: 29,
+    caption: "Exclusively for teams",
+    features: [
+      "All Overdrive features",
+      "10TB additional storage",
+      "50% off per member",
+      "Real-time collaboration",
+    ],
+    icon: "/images/hexagon.svg",
+    logo: "/images/plan-3.png",
+  },
+];
+
+export const testimonials = [
+  {
+    id: "0",
+    name: "Jessica Saunders",
+    role: "Globalnomads",
+    avatarUrl: "/images/testimonials/jessica-saunders.png",
+    comment:
+      "Xora's customer support is second to none! They’re like my tech superheroes, always there when I need them.",
+  },
+  {
+    id: "1",
+    name: "Mark Erixon",
+    role: "Vid capital intl",
+    avatarUrl: "/images/testimonials/mark-erixon.png",
+    comment:
+      "I was skeptical at first, but now I can't imagine our content operations without it. It's that impactful.",
+  },
+  {
+    id: "2",
+    name: "Melanie Hurst",
+    role: "Cyberleap",
+    avatarUrl: "/images/testimonials/melanie-hurst.png",
+    comment:
+      "Adopting this software was a breeze. It's made everyday tasks so much simpler.",
+  },
+  {
+    id: "3",
+    name: "Alicia Barker",
+    role: "Cyberleap",
+    avatarUrl: "/images/testimonials/alicia-barker.png",
+    comment:
+      "The analytics feature is like having a personal fortune teller. It's been instrumental in guiding our business decisions.",
+  },
+  {
+    id: "4",
+    name: "Becky Snider",
+    role: "Floclips",
+    avatarUrl: "/images/testimonials/becky-snider.png",
+    comment:
+      "Switched to Xora last month, and I'm already seeing results. Best decision for our team!",
+  },
+  {
+    id: "5",
+    name: "Jim Bradley",
+    role: "Vid capital intl",
+    avatarUrl: "/images/testimonials/jim-bradley.png",
+    comment:
+      "The efficiency boost is undeniable. This platform has transformed our workflow, forever.",
+  },
+];
+
+export const logos = [
+  {
+    id: "0",
+    title: "Afterpay",
+    url: "/images/logos/afterpay.svg",
+    width: 156,
+    height: 48,
+  },
+  {
+    id: "1",
+    title: "Amplitude",
+    url: "/images/logos/amplitude.svg",
+    width: 194,
+    height: 48,
+  },
+  {
+    id: "2",
+    title: "Sonos",
+    url: "/images/logos/sonos.svg",
+    width: 115,
+    height: 48,
+  },
+  {
+    id: "3",
+    title: "Maze",
+    url: "/images/logos/maze.svg",
+    width: 142,
+    height: 48,
+  },
+  {
+    id: "4",
+    title: "Drips",
+    url: "/images/logos/drips.svg",
+    width: 77,
+    height: 48,
+  },
+];
+
+// export const Ios = () => {
+//   return (
+//     <svg
+//       width="32"
+//       height="32"
+//       viewBox="0 0 32 32"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg">
+//       <path
+//         d="M24.9404 17.0175C24.9566 15.791 25.2903 14.5884 25.9105 13.5217C26.5307 12.4549 27.4173 11.5586 28.4876 10.9162C27.8077 9.96818 26.9106 9.18798 25.8677 8.63759C24.8249 8.0872 23.6649 7.78178 22.48 7.74559C19.9523 7.48658 17.5019 9.22215 16.2138 9.22215C14.9009 9.22215 12.9177 7.77131 10.7822 7.8142C9.40087 7.85777 8.05467 8.2499 6.87475 8.95239C5.69483 9.65487 4.72143 10.6438 4.04939 11.8227C1.13826 16.7431 3.3097 23.9744 6.09832 27.9516C7.49352 29.8992 9.12411 32.0746 11.2577 31.9975C13.3456 31.913 14.1253 30.6978 16.6456 30.6978C19.1424 30.6978 19.874 31.9975 22.0509 31.9484C24.2912 31.9129 25.7028 29.9922 27.049 28.0262C28.0514 26.6385 28.8228 25.1048 29.3345 23.4819C28.0329 22.9445 26.9222 22.0449 26.1408 20.8954C25.3594 19.7458 24.942 18.3971 24.9404 17.0175Z"
+//         fill="#EAEDFF"
+//       />
+//       <path
+//         d="M20.829 5.12933C22.0505 3.69777 22.6523 1.85774 22.5066 0C20.6403 0.191354 18.9165 1.0621 17.6784 2.43873C17.0731 3.11126 16.6095 3.89365 16.3141 4.74119C16.0187 5.58873 15.8973 6.4848 15.9569 7.37817C16.8903 7.38755 17.8138 7.19004 18.6577 6.8005C19.5017 6.41097 20.244 5.83956 20.829 5.12933Z"
+//         fill="#EAEDFF"
+//       />
+//     </svg>
+//   );
+// };
+// ;
+
+// export const Android = () => {
+//   return (
+//     <svg
+//       width="40"
+//       height="40"
+//       viewBox="0 0 40 40"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg">
+//       <path
+//         d="M28.685 28.977C27.661 28.977 26.841 29.797 26.841 30.821V38.565C26.841 39.589 27.661 40.409 28.685 40.409C29.709 40.409 30.529 39.589 30.529 38.565V30.821C30.529 29.797 29.709 28.977 28.685 28.977Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M7.37 28.977C6.346 28.977 5.526 29.797 5.526 30.821V38.565C5.526 39.589 6.346 40.409 7.37 40.409C8.394 40.409 9.214 39.589 9.214 38.565V30.821C9.214 29.797 8.394 28.977 7.37 28.977Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M32.5 12.5H5.812C2.78 12.5 0 15.28 0 18.312V28.156C0 31.188 2.78 33.968 5.812 33.968H32.5C35.532 33.968 38.312 31.188 38.312 28.156V18.312C38.312 15.28 35.532 12.5 32.5 12.5Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M9.12 8.788C8.56 8.788 8.055 9.041 7.734 9.362C7.413 9.683 7.16 10.188 7.16 10.748C7.16 11.308 7.413 11.813 7.734 12.134C8.055 12.455 8.56 12.708 9.12 12.708C9.68 12.708 10.185 12.455 10.506 12.134C10.827 11.813 11.08 11.308 11.08 10.748C11.08 10.188 10.827 9.683 10.506 9.362C10.185 9.041 9.68 8.788 9.12 8.788Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M28.88 8.788C28.32 8.788 27.815 9.041 27.494 9.362C27.173 9.683 26.92 10.188 26.92 10.748C26.92 11.308 27.173 11.813 27.494 12.134C27.815 12.455 28.32 12.708 28.88 12.708C29.44 12.708 29.945 12.455 30.266 12.134C30.587 11.813 30.84 11.308 30.84 10.748C30.84 10.188 30.587 9.683 30.266 9.362C29.945 9.041 29.44 8.788 28.88 8.788Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M22.285 0.799L24.153 2.666C22.658 3.531 21.352 4.628 20.285 5.905L18.311 3.931C19.481 2.517 20.858 1.322 22.285 0.349V0.799Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M14.941 2.666L16.913 0.694C18.287 1.625 19.59 2.685 20.75 3.849L18.821 5.876C17.755 4.599 16.448 3.515 14.941 2.666V2.666Z"
+//         fill="#E0E4FF"
+//       />
+//     </svg>
+//   );
+// };
+
+// export const Web = () => {
+//   return (
+//     <svg
+//       width="40"
+//       height="40"
+//       viewBox="0 0 40 40"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg">
+//       <path
+//         d="M14.743 1.059C13.937 3.699 13.395 6.444 13.021 9.188C17.557 8.68 22.129 8.68 26.665 9.188C26.291 6.444 25.75 3.713 24.943 1.059C24.917 0.943 24.908 0.854 24.9 0.759C24.896 0.708 24.892 0.655 24.885 0.607C23.195 0.24 21.355 0 19.5 0C17.627 0 15.805 0.24 14.042 0.607C14.035 0.674 14.033 0.732 14.031 0.788C14.029 0.872 14.027 0.953 14.743 1.059Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M29.785 9.03C32.456 9.675 35.092 10.408 37.715 11.256C35.176 7.279 31.352 4.203 27.375 2.329C28.198 5.279 28.842 8.323 29.785 9.03Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M11.925 36.181C11.896 36.181 11.863 36.198 11.83 36.215C11.797 36.232 11.764 36.249 11.736 36.249C7.845 34.688 4.685 31.856 2.742 27.884C2.742 27.856 2.758 27.823 2.773 27.79C2.788 27.757 2.803 27.724 2.803 27.697C5.225 28.399 7.706 28.897 10.166 29.249C10.845 31.889 11.625 34.528 11.925 36.181Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M37.625 27.907C35.086 31.884 31.086 34.901 27.375 36.516C28.213 33.551 28.842 30.579 29.785 27.697C32.245 27.345 34.685 26.847 37.125 26.255C37.11 26.298 37.125 26.339 37.137 26.376C37.153 26.409 37.167 26.441 37.167 26.472Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M11.925 2.178C10.982 5.259 10.166 8.273 9.577 11.394C6.895 11.81 4.214 12.434 1.545 13.286C3.485 9.24 7.705 6.105 11.736 4.24C11.764 4.24 11.797 4.259 11.83 4.278C11.863 4.296 11.896 4.315 11.925 4.315Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M9.83 26.979C7.045 26.445 4.29 25.59 1.545 24.398C1.555 24.477 1.565 24.554 1.576 24.634C1.588 24.713 1.599 24.792 1.609 24.866C3.61 28.947 7.9 32.011 11.87 33.968C11.897 33.968 11.931 33.94 11.96 33.916C11.988 33.891 12.015 33.866 12.043 33.84C11.586 31.591 10.798 29.285 9.83 26.979Z"
+//         fill="#E0E4FF"
+//       />
+//       <path
+//         d="M27.393 10.169C22.489 9.589 17.549 9.589 12.645 10.169C11.877 14.109 11.878 18.485 12.645 22.425C17.549 23.005 22.489 23.005 27.393 22.425C28.16 18.485 28.16 14.109 27.393 10.169Z"
+//         fill="#E0E4FF"
+//       />
+//     </svg>
+//   );
+// };
+
+
+// export const links = [
+//   {
+//     id: "0",
+//     title: "Ios",
+//     icon: <Ios />,
+//     url: "#",
+//   },
+//   {
+//     id: "1",
+//     title: "Android",
+//     icon: <Android />,
+//     url: "#",
+//   },
+//   {
+//     id: "2",
+//     title: "Windows",
+//     icon: <Windows />,
+//     url: "#",
+//   },
+//   {
+//     id: "3",
+//     title: "Web",
+//     icon: <Web />,
+//     url: "#",
+//   },
+// ];
+
+export const socials = [
+  {
+    id: "0",
+    title: "x",
+    icon: "/images/socials/x.svg",
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Threads",
+    icon: "/images/socials/threads.svg",
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    icon: "/images/socials/instagram.svg",
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Discord",
+    icon: "/images/socials/discord.svg",
+    url: "#",
+  },
+];
